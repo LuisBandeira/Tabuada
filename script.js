cbe488c71c72fef75bbad7abd9bb2bfb917f8631
LuisBandeira <LpBandeira.contato@gmail.com>
2021-10-11 11:15:46 -0300

    Estilo.Css
    Aqui foi feito toda a configuração visual da tabuada

---
+++ script.js
@@ -0,0 +1,25 @@
+function gerar()
+{
+    let num = document.getElementById('tnum')
+    let tab = document.getElementById('seltab')
+    
+    if(num.value.length == 0)
+    {
+        window.alert('Por favor, insira um numero!')
+    }else{
+        let n = Number(num.value)
+        let c =1
+        tab.innerHTML = ''
+
+        while(c<=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            item.value = `tab${c}`
+            c++
+            
+        }
+        
+    }
+    
+}
